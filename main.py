c4cfceb2d7a1378af4bbefc7b565646c68ab61ed
CampNoNet <bob92588@gmail.com>
2022-11-02 17:51:19 -0400

    Main Program Beta V .01

---
+++ main.py
@@ -0,0 +1,35 @@
+import qrcode
+class qr:
+    def __init__(self, data):
+        self.data = data
+        self.a = ''
+
+    def fname(self):
+        name = self.name
+        rip = self.data.split()
+        for i in range(len(rip)):
+            name = name + rip[i]
+        name = name + '.png'
+        return a
+    
+    def generate_qr(self, save):
+        qr = qrcode.QRCode(
+            version = 1,
+            error_correction = qrcode.constants.ERROR_CORRECT_L,
+            box_size = 10, 
+            border = 4,
+        )
+        qr.add_data(self.data)
+        qr.make(fit = True)
+        img = qr.make_image(fill_color="black", back_color="white")
+        name = self.fname()
+        if save.strip() == 'True' or save.strip() == "T":
+            img.save(name)
+            print("Saving...")
+        
+        
+        return img
+if __name__ == '__main__':
+    usr = input("Please Enter the QR code Value, follwed True or False:  ")
+    i_put  = usr.split(',')
+    qr(i_put[0]).generate_qr(i_put[1])
