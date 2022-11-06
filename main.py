53c8b3f04f4b1890cbff7a9d4bbcf48304bc8deb
CampNoNet <bob92588@gmail.com>
2022-11-06 12:44:16 -0500

    Code Changes

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import qrcode
+from time import strftime, gmtime
 class qr:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self):
+
         self.name = ''
 
     def fname(self):
-        name = self.name
+        
         rip = self.data.split() # Spilts the QR input by whitespace
         for i in range(len(rip)): # for loop, to cycle through rip then builds a file name 
-            name = name + rip[i]
-        name = name + '.png'
-        return name # returns the name of the file
+            if i > 2:
+                break
+            self.name = self.name + rip[i]
+        self.name = self.name + strftime('%m%d%y',gmtime()) + '.png'
+        return self.name # returns the name of the file
     
-    def generate_qr(self, save):
+    def generate_qr(self, data, save):
+        self.data = data
         qr = qrcode.QRCode(
             version = 1,
             error_correction = qrcode.constants.ERROR_CORRECT_L,
@@ -23,10 +27,13 @@ class qr:
         qr.make(fit = True)
         img = qr.make_image(fill_color="black", back_color="white") # Photo color settings 
         name = self.fname() # Call function to provide a file name 
-        if save.strip() == 'True' or save.strip() == "T": # Checks to see if the img should be save or not
+        
+        if save == 1:
+            print("saving img ")
             img.save(name)        
+        
         return img
 if __name__ == '__main__':
     usr = input("Please Enter the QR code Value, follwed True or False:  ")
     i_put  = usr.split(',')
-    qr(i_put[0]).generate_qr(i_put[1])
+    print(qr().generate_qr('Sam Mesic is the best', 0))
